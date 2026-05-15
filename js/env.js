2423eeb6245c2fad7b56c82136e0352af19c3086
M.ABO-ELNASR <mohamedaboelnasr500@gmail.com>
2026-05-15 12:56:28 +0300

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbxDUHRB5us53hqHg3i8WMS95aFKJK7qdzGE441hwr-PtHD_crZBxe4k5WFMyfNSEFTp1Q/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbymbNtQyfZtmPwdzCstvyAIZ2IxHKqT83BdVRHBS1crfPSGonkx-6WQOQ-21TJ4fJPxlg/exec';
 
 const VAPID_PUBLIC_KEY = 'BFVFu7GzLkcwF-hCA5yVO7T5H49FpMp9hUyeBaF3QjtG-BaDxstIYU-rs8w8S2B6-rdEvBlbYsOQhoNqa7lpl3o';
 
